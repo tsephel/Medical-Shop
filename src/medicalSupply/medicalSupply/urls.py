827a0acfe574bf45f17fe75d5471a2ac1ca92e7c
tsephel <ngawangkunga01@gmail.com>
2021-06-20 12:47:59 +0545

    Cart model added

--- src/medicalSupply/medicalSupply/urls.py
+++ src/medicalSupply/medicalSupply/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('cart.urls')),
 ]
 
 if settings.DEBUG:
--- src/medicalSupply/store/views.py
+++ src/medicalSupply/store/views.py
@@ -6,7 +6,7 @@ from category.models import Category
 def store(request, category_slug=None):
     categories = None
     products = None
-
+    
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
@@ -20,8 +20,9 @@ def store(request, category_slug=None):
         'count': count,
     }
     
-    return render(request, 'store.html', context)
+    return render(request, 'store/store.html', context)
 
+#method to display the single product with category and product slug
 def prodcut_detail(request, category_slug, product_slug):
     try:
         detail = Product.objects.get(category__slug=category_slug, slug=product_slug)
@@ -32,4 +33,4 @@ def prodcut_detail(request, category_slug, product_slug):
         'detail': detail,
     }
 
-    return render(request, 'product_detail.html', context)
+    return render(request, 'store/product_detail.html', context)
