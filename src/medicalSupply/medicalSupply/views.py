bd79c2f9116fd1c08e2d95330067411816578818
tsephel <ngawangkunga01@gmail.com>
2021-06-23 12:15:21 +0545

    banner added and some design changed for the detail page

--- src/medicalSupply/medicalSupply/views.py
+++ src/medicalSupply/medicalSupply/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from store.models import Product
+from category.models import Category
 
 def home(request):
 	products = Product.objects.all().filter(is_available=True)
+	category = Category.objects.all()[:3]
 
 	context = {
 		'products': products,
+		'category': category,
 	}
 
 	return render(request, 'home.html', context);
